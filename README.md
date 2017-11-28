d43179570d5b88da289b8bd42e0a345bcd4932f6
Damien <email@example.com>
2017-11-28 14:47:14 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# lcd_tomcat
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: lcd_tomcat
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
